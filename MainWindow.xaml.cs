c8ed6559ea0c905d602f26810796c90935ea5967
TomasHanousek1 <tomas.hanousek8@seznam.cz>
2022-02-07 11:04:33 +0100

    FinalVersion

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,17 +13,21 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
+using System.IO;
 
 namespace DU_Binding
 {
     class Osoba : INotifyPropertyChanged
     {
-        private string _Jmeno, _Prijmeni;
-        private DateTime _Narozeni;
+        public string _Jmeno, _Prijmeni;
+        public DateTime _Narozeni;
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // Při každé změně dat chceme akutalizovat status bar
+        public Osoba(string jmeno, string prijmeni, DateTime narozeni)
+        {
+            _Jmeno = jmeno;
+            _Prijmeni = prijmeni;
+            _Narozeni = narozeni;
+        }
         public string Jmeno
         {
             get => _Jmeno;
@@ -55,30 +59,80 @@ namespace DU_Binding
             }
         }
 
-        public string Status
-        {
-            get => Jmeno + " " + Prijmeni + " " + Narozeni.ToString();
-        }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public override string ToString()
-        {
-            return Jmeno + " " + Prijmeni + " " + Narozeni.ToShortDateString();
-        }
-
-        // pomocná metoda pro informaci o změně v datech
         private void OnPropertyChanged(string property)
         {
             if (PropertyChanged != null) // jestli někdo poslouchá ...
                 PropertyChanged(this, new PropertyChangedEventArgs(property));
         }
     }
-    class Zamestnanec : Osoba
+    class Zamestnanec : Osoba, INotifyPropertyChanged
     {
-        enum Vzdelani{ ZS, SS, VS }
-        string PracovniPozice;
-        double Plat;
+        public enum Vzdelani{ ZS, SS, VS }
+        public Vzdelani _TvojeVzdelani;
+        public string _PracovniPozice;
+        public int _Plat;
 
-        
+        public Zamestnanec(string jmeno, string prijimeni, DateTime narozeniny, Vzdelani tvojeVzdelani, string pracovniPozice, int plat) : base(jmeno, prijimeni, narozeniny)
+        {
+            _TvojeVzdelani = tvojeVzdelani;
+            _PracovniPozice = pracovniPozice;
+            _Plat = plat;
+        }
+
+
+        // Při každé změně dat chceme akutalizovat status bar
+        public Vzdelani TvojeVzdelani
+        {
+            get => _TvojeVzdelani;
+            set
+            {
+                _TvojeVzdelani = value;
+                OnPropertyChanged("TvojeVzdelani");
+                OnPropertyChanged("Status");
+            }
+        }
+        public string PracovniPozice
+        {
+            get => _PracovniPozice;
+            set
+            {
+                _PracovniPozice = value;
+                OnPropertyChanged("PracovniPozice");
+                OnPropertyChanged("Status");
+            }
+        }
+        public int Plat
+        {
+            get => _Plat;
+            set
+            {
+                _Plat = value;
+                OnPropertyChanged("Plat");
+                OnPropertyChanged("Status");
+            }
+        }
+
+
+         public string Status
+        {
+            get => Jmeno + " " + Prijmeni + " " + Narozeni.ToShortDateString() + " " + TvojeVzdelani + " " + PracovniPozice + " " + Plat + "Kč";
+        } 
+
+        public override string ToString()
+        {
+            return Jmeno + " " + Prijmeni + " " + Narozeni.ToShortDateString() + " " + TvojeVzdelani + " " + PracovniPozice + " " + Plat + "Kč";
+        }
+
+        // pomocná metoda pro informaci o změně v datech
+
+        public event PropertyChangedEventHandler PropertyChangedZ;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChangedZ != null) // jestli někdo poslouchá ...
+                PropertyChangedZ(this, new PropertyChangedEventArgs(property));
+        }
     }
 
     /// <summary>
@@ -86,34 +140,45 @@ namespace DU_Binding
     /// </summary>
     public partial class MainWindow : Window
     {
-        Osoba p;
+        Zamestnanec z;
 
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext =
-            p = new Osoba()
-            {
-                Narozeni = DateTime.Now
-            };
-
+             z = new Zamestnanec(tbJmeno.Text, tbPrijmeni.Text, DateTime.Now, 0, tbPracovniPozice.Text, 0)
+             {
+                 Narozeni = DateTime.Now
+             };
         }
 
-        private void BtShow_Click(object sender, RoutedEventArgs e)
-        {
-            BindingExpression expr = tbPrijmeni.GetBindingExpression(TextBox.TextProperty);
-            expr?.UpdateSource();
-
-            MessageBox.Show(p.ToString() + "\n" +
-                expr.ResolvedSourcePropertyName + ": vynucená akutalizace");
-        }
+        
 
         // Změna dat, vzhledem k bindingu, stačí i k aktualizaci grafické podoby formuláře
         private void BtClear_Click(object sender, RoutedEventArgs e)
         {
-            p.Jmeno = p.Prijmeni = string.Empty;
-            p.Narozeni = DateTime.Now;
+            z.Jmeno = z.Prijmeni = z.PracovniPozice = string.Empty;
+            ComboNejvyssiVzdelani.Text = tbPracovniPozice.Text = "";
+            tbPlat.Text = "";
+            z.Plat = 0;
+            z.Narozeni = DateTime.Now;
+        }
+
+        private void btSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (z.Plat > 0 && z.Jmeno != "" && z.Prijmeni != "" && z.PracovniPozice != "" && ComboNejvyssiVzdelani.Text != "")
+            {
+                using (StreamWriter sw = new StreamWriter("MojeData.txt", true))
+                {
+                    sw.WriteLine(z.ToString());
+                    sw.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Něco jsi nevyplnil" + "\n");
+            }
         }
     }
 
